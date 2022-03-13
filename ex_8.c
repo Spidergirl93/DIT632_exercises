2a2feb1567fd5df59d0c21df66f6bbf7f5fab298
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-03-13 12:30:36 +0100

    exercise 8  not finished

---
+++ ex_8.c
@@ -0,0 +1,60 @@
+//
+// Created by marye on 2022-03-12.
+//
+
+/**
+ * Write a program in C to print all permutations of a given string using pointers.
+   Expected Output :
+     The permutations of the string are :
+     abcd abdc acbd acdb adcb adbc bacd badc bcad bcda
+     bdca bdac cbad cbda cabd cadb cdab cdba dbca dbac
+     dcba dcab dacb dabc
+ */
+
+
+
+// C program to print all permutations with duplicates allowed
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void permutate(char *word);
+
+int factorial(int n);
+
+int main()
+{
+    char given_string[] = "abcd";
+
+    permutate(given_string);
+}
+
+void permutate(char *word) {
+
+    int n = strlen(word);
+
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = i; j < n; j++)
+        {
+            char copy_word[n];
+            strncpy(copy_word, word, n);
+            char temp = copy_word[i];
+            copy_word[i] =copy_word[j];
+            copy_word[j] = temp;
+            printf("%s  ", copy_word);
+        }
+    }
+}
+
+int factorial(int n) {
+    int result = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        result = i * result;
+    }
+    return result;
+}
+
+
+
