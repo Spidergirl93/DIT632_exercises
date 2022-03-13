4673094b4b27a23677e0973f8cae9d5f3a1f35ac
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-03-13 13:14:12 +0100

    exercise 9 added

--- ex_9.c
+++ ex_9.c
@@ -5,7 +5,9 @@
  * Write a program in C to count the number of vowels and consonants in a string using a pointer.
  * Test Data :
  * Input a string:
- * string Expected Output :
+ * string
+ *
+ * Expected Output :
  * Number of vowels : 1
  * Number of consonants : 5
  */
